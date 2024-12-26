1f63b0c08044c1c41c09a09b7f5026212edd0a75
Riccardo Consolandi <rconsolandi@gmail.com>
2024-12-26 11:13:13 +0100

    fix: (description) refactoring component for improved readability and maintainability

--- app/components/description.tsx
+++ app/components/description.tsx
@@ -1,10 +1,17 @@
 import { DescriptionProps } from "../types/types";
+
 const Description = ({ summaries, currentIndex }: DescriptionProps) => {
+  if (!summaries || currentIndex < 0 || currentIndex >= summaries.length) {
+    return null;
+  }
+
+  const { description, link } = summaries[currentIndex];
+
   return (
     <p className="mt-8 text-left sm:text-left text-lg sm:text-xl text-gray-600 font-medium mb-4">
-      {summaries[currentIndex].description.split(/(\/.*?\/)/g).map((part, index) =>
+      {description.split(/(\/.*?\/)/g).map((part, index) =>
         part.startsWith("/") && part.endsWith("/") ? (
-          <a key={index} href={summaries[currentIndex].link || "#"} target="_blank" rel="noopener noreferrer" className="text-blue-600 underline">
+          <a key={index} href={link || "#"} target="_blank" rel="noopener noreferrer" className="text-blue-600 underline">
             {part.slice(1, -1)}
           </a>
         ) : (
