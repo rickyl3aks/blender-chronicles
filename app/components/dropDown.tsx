b158036d75dc913a786933ab206919167729084b
Riccardo Consolandi <rconsolandi@gmail.com>
2025-05-26 15:58:21 +0100

    fix: adjust dropdown component structure

--- app/components/dropDown.tsx
+++ app/components/dropDown.tsx
@@ -24,7 +24,7 @@ export default function DropDown() {
     <div ref={dropdownRef} className="relative inline-block">
       <button onClick={() => setOpen((prev) => !prev)} className="btn-projects flex items-center gap-2 relative overflow-hidden px-4 py-2">
         projects
-        <span className="circle-arrow flex items-center justify-center w-6 h-6 rounded-full border-2 border-black transition-transform duration-300 hover:translate-y-1">
+        <span className="circle-arrow ">
           <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-black" fill="none" viewBox="0 0 24 24" stroke="white" strokeWidth={3}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
           </svg>
@@ -32,22 +32,16 @@ export default function DropDown() {
       </button>
 
       {open && (
-        <ul className="absolute mt-2 left-0 w-48 bg-white border border-black shadow-lg z-10 rounded-lg overflow-hidden">
-          <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer">
-            <Link href="/rendering" onClick={() => setOpen(false)}>
-              3D Render
-            </Link>
-          </li>
-          <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer">
-            <Link href="/lowPoly" onClick={() => setOpen(false)}>
-              Low Poly
-            </Link>
-          </li>
-          <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer">
-            <Link href="/motionGraphics" onClick={() => setOpen(false)}>
-              Motion Graphics
-            </Link>
-          </li>
+        <ul className="absolute mt-2 left-0 w-48 bg-white border border-black shadow-lg z-10 rounded-lg overflow-hidden border-[3px] border-black">
+          <Link href="/rendering">
+            <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer">3D Render</li>
+          </Link>
+          <Link href="/lowPoly">
+            <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer">Low Poly</li>
+          </Link>
+          <Link href="/motionGraphics">
+            <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer">Motion Graphics</li>
+          </Link>
         </ul>
       )}
     </div>
