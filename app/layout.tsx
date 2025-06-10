813aca2e0d110f4811d3b5b2bf5636ff9b51cf93
Riccardo Consolandi <rconsolandi@gmail.com>
2025-06-10 16:52:58 +0100

    fix: (layout) adjust bg for firefox mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-
 import type { Metadata } from "next";
 import "./globals.css";
 import { Manrope } from "next/font/google";
@@ -26,8 +24,14 @@ export default function RootLayout({
     <html lang="en" className={manrope.className}>
       <body className="text-regular bg-[#f1ebde]">
         <div className="relative min-h-screen">
-          <section className="min-h-screen w-full fixed top-0 left-0 -z-10 [background-image:repeating-linear-gradient(-45deg,transparent_0,transparent_28%,#c7c7c7_28%,#c7c7c7_35%,#f1ebde_35%,#f1ebde_38%,#c7c7c7_38%,#c7c7c7_43%,#f1ebde_43%,#f1ebde_46%,#c7c7c7_46%,#c7c7c7_52%,#f1ebde_52%,#f1ebde_56%,#c7c7c7_56%,#c7c7c7_64%,transparent_64%)]"></section>
-          <div className="relative z-0">
+          <section
+            className="fixed top-0 left-0 w-full h-screen z-0"
+            style={{
+              backgroundImage:
+                "repeating-linear-gradient(-45deg, transparent 0, transparent 28%, #c7c7c7 28%, #c7c7c7 35%, #f1ebde 35%, #f1ebde 38%, #c7c7c7 38%, #c7c7c7 43%, #f1ebde 43%, #f1ebde 46%, #c7c7c7 46%, #c7c7c7 52%, #f1ebde 52%, #f1ebde 56%, #c7c7c7 56%, #c7c7c7 64%, transparent 64%)",
+            }}
+          />
+          <div className="relative z-10">
             <Navbar />
             <main className="min-h-screen pt-[10rem] max-w-[1200px] mx-auto">{children}</main>
             <Footer />
